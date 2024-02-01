b11dde372c094df090ab0b2ddcdfab3eab582b65
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-01 13:57:46 +0530

    header

--- foodcourt/src/components/Header.js
+++ foodcourt/src/components/Header.js
@@ -2,13 +2,14 @@ import React from 'react'
 import logo from "../assets/logo.png"
 const Header = () => {
     return (
-        <div className=''>
+        <div className='flex  justify-between px-20 py-3 items-center bg-gradient-to-l from-[#FFD5D4] to-[#FFF4F3]'>
             <div><img src={logo} alt='logo' /></div>
-            <div>
-                <li>Why foodcourt ?</li>
+            <div className='list-none flex gap-3 text-lg items-center'>
+                <li className='text-[#FF5454]'>Why foodcourt ?</li>
+                <li>Services</li>
                 <li>Menu</li>
                 <li>Contact</li>
-                <li>Login</li>
+                <li className='bg-[#FF5454] py-2 rounded-full px-4 text-white'>Login</li>
 
             </div>
         </div>
