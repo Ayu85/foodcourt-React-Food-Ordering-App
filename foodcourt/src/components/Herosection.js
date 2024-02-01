94482a0937427c164f79a3e7e62e93df12c85a10
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-01 14:32:41 +0530

    little responsive

--- foodcourt/src/components/Herosection.js
+++ foodcourt/src/components/Herosection.js
@@ -2,8 +2,8 @@ import React from 'react'
 import herobg from "../assets/herobg.png"
 const Herosection = () => {
     return (
-        <div className=' bg-gradient-to-l from-[#FFD5D4] to-[#FFF4F3] h-[100vh] flex justify-between items-center px-20'>
-            <div className=' max-w-[500px] '>
+        <div className=' bg-gradient-to-l from-[#FFD5D4] to-[#FFF4F3] gap-5 h-[100vh] flex justify-between items-center '>
+            <div className=' max-w-[500px] pl-20 '>
                 <h1 className=' text-6xl font-semibold '>Fastest <span className='text-[#FF5454]'>Food Delivery</span> & Easy Pickup</h1>
                 <p className='italic pt-4'>"Welcome to FlavorFusionHub - your ultimate destination for culinary delight! Discover a world of flavors, from savory to sweet, all in one place. Whether you're craving classics or eager to explore new tastes,
                     our app brings the best of dining right to your fingertips. Bon appétit!"
@@ -14,7 +14,7 @@ const Herosection = () => {
 
                 </buttonsection>
             </div>
-            <div className='h-full bg-[#FCD3D1] rounded-l-full'>
+            <div className='h-full bg-[#FCD3D1] rounded-l-full lg:block hidden'>
                 <img src={herobg} alt='logo' className='h-full ' />
             </div>
         </div>
