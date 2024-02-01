b6fad44b20ab528b6c55f92736dd4d9217d4fb16
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-01 22:28:10 +0530

    responsive

--- foodcourt/src/components/Herosection.js
+++ foodcourt/src/components/Herosection.js
@@ -9,9 +9,9 @@ const Herosection = () => {
         <div className='relative bg-gradient-to-l from-[#FFD5D4] to-[#FFF4F3] gap-5 h-[100vh] flex justify-between items-center '>
             <div className='absolute top-44  -left-14'><TbChartDonutFilled className='text-9xl text-[#f8b5ad] animate-[spin_4s_ease-in-out_infinite] ' />
             </div>
-            <div className='absolute top-20  lg:right-[750px] sm:right-44 right-44'><ImLocation2 className='text-8xl text-[#F64E61] animate-[bounce_2s_ease-in-out_infinite] ' />
+            <div className='absolute top-20  lg:right-[750px] sm:right-44 right-44 hidden md:block lg:block'><ImLocation2 className='text-8xl text-[#F64E61] animate-[bounce_2s_ease-in-out_infinite] ' />
             </div>
-            <div className=' lg:max-w-[500px] lg:pl-20 sm:pr-10 sm:pl-10 pl-10 z-50 '>
+            <div className=' lg:max-w-[500px] lg:pl-20 sm:pr-14 sm:pl-10  pl-5 pr-5 z-50 '>
                 <h1 className=' text-6xl font-semibold '>Fastest <span className='text-[#FF5454]'>Food Delivery</span> & Easy Pickup</h1>
                 <p className='italic pt-4'>"Welcome to FlavorFusionHub - your ultimate destination for culinary delight! Discover a world of flavors, from savory to sweet, all in one place. Whether you're craving classics or eager to explore new tastes,
                     our app brings the best of dining right to your fingertips. Bon appétit!"
