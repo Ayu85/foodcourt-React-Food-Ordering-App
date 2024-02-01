e44c0c8c9a35a9471718ea9383a37a4f5dd122ca
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-01 13:51:28 +0530

    tailwind css installed

--- foodcourt/src/components/Header.js
+++ foodcourt/src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import logo from "../assets/logo.png"
 const Header = () => {
     return (
-        <div>
-           <img src=''/>
+        <div className=''>
+            <div><img src={logo} alt='logo' /></div>
+            <div>
+                <li>Why foodcourt ?</li>
+                <li>Menu</li>
+                <li>Contact</li>
+                <li>Login</li>
+
+            </div>
         </div>
     )
 }
