145ec2dd434bcdffbec867795f801ca6dcad53b0
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-02 22:34:13 +0530

    styling

--- foodcourt/src/components/Herosection.js
+++ foodcourt/src/components/Herosection.js
@@ -10,12 +10,12 @@ import { IoMdLogIn } from "react-icons/io";
 const Herosection = () => {
     const showHamburgermenu = useSelector(store => store.hamburgerslice.menu)
     return (
-        <div className='relative bg-gradient-to-l from-[#FFD5D4] to-[#FFF4F3] gap-5 h-[100vh] flex justify-between items-center '>
+        <div className='relative bg-gradient-to-l cd f  from-[#FFD5D4] to-[#FFF4F3] gap-5 h-[100vh] flex justify-between items-center '>
             <div className='absolute top-44  -left-14'><TbChartDonutFilled className='text-9xl text-[#f8b5ad] animate-[spin_4s_ease-in-out_infinite] ' />
             </div>
             <div className='absolute top-10  lg:right-[750px] sm:right-44 right-44 hidden md:block lg:block'><ImLocation2 className='text-8xl text-[#F64E61] animate-[bounce_2s_ease-in-out_infinite] ' />
             </div>
-            <div className=' lg:w-[600px] lg:pl-20 sm:pr-14 sm:pl-10  pl-5 pr-5 z-40 sm:-mt-20  -mt-20'>
+            <div className=' lg:w-[600px] lg:pl-20 sm:pr-14 sm:pl-10 pl-5 pr-5 z-40 sm:-mt-20  -mt-20'>
                 <h1 className=' text-6xl font-semibold '>Fastest <span className='text-[#FF5454]'>Food Delivery</span> & Easy Pickup</h1>
                 <p className='italic pt-4'>"Welcome to FlavorFusionHub - your ultimate destination for culinary delight! Discover a world of flavors, from savory to sweet, all in one place. Whether you're craving classics or eager to explore new tastes,
                     our app brings the best of dining right to your fingertips. Bon appétit!"
