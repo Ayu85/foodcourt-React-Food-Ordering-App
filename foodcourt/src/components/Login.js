58911e93cbe6a0884f8e2f1c362d62073cae573d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-02 23:52:08 +0530

    login page

--- foodcourt/src/components/Login.js
+++ foodcourt/src/components/Login.js
@@ -3,15 +3,13 @@ import logo from "../assets/logo.png"
 import bg from "../assets/loginBg.jpg"
 const Login = () => {
     return (
-        <div className='  gap-5 h-[100vh] bg-hero' >
-            <nav>
-                <img src={logo} alt='logo' />
-            </nav>
-            <div className='flex items-center flex-col justify-center bg-[#ffd5d481]  backdrop-blur-sm w-[450px] rounded-lg ml-[50%] -translate-x-[50%] translate-y-[20%]'>
-                <h1 className='text-3xl text-zinc-900 font-semibold py-3  '>Login</h1>
-                <form className='flex flex-col gap-10 px-10 py-5 '>
+        <div className='  gap-3 h-[100vh] bg-hero bg-cover filter flex flex-col items-center justify-center' >
+            <h1 className='text-4xl text-black font-serif font-semibold py-3  '>Login</h1>
+            <h1>More than <span className='text-[#FF5454] font-semibold'>15000+ Recipes</span> around the world</h1>
+            <div className='flex items-center flex-col justify-center bg-[#ffd5d481] bg-white  backdrop-blur-sm w-[450px] rounded-lg'>
+                <form className='flex flex-col gap-10 px-10 py-20 '>
                     <input type='mail' placeholder='Enter email ' className='py-3 w-72 rounded-md pl-2 placeholder-shown:bg-[#FFF4F3]' />
-                    <input type='password' placeholder='Enter password ' className='py-3 w-72 rounded-md pl-2' />
+                    <input type='password' placeholder='Enter password ' className='py-3 w-72 rounded-md pl-2 placeholder-shown:bg-[#FFF4F3]' />
                     <button className='py-3 w-72 rounded-md bg-[#FF5454] text-white'>Login</button>
                 </form>
             </div >
--- foodcourt/tailwind.config.js
+++ foodcourt/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        hero: "url('../foodcourt/src/assets/loginBg.jpg')"
+        hero: "url('https://img.freepik.com/free-photo/top-view-ramen-soup-dumplings-with-copy-space_23-2148368678.jpg?w=996&t=st=1706897698~exp=1706898298~hmac=fb0f879e2eacbcc3e5e092b04b591553e913c2130ac90ac61229bdf3e31098b2')"
       }
     },
   },
