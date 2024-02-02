79140664031526249974e9ac127621e52722ade5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-03 00:11:58 +0530

    login and sign up

--- foodcourt/src/components/Login.js
+++ foodcourt/src/components/Login.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/logo.png"
 import bg from "../assets/loginBg.jpg"
 const Login = () => {
+    const [isSignin, setSignin] = useState(false)
     return (
-        <div className='  gap-3 h-[100vh] bg-hero bg-cover filter flex flex-col items-center justify-center' >
-            <h1 className='text-4xl text-black font-serif font-semibold py-3  '>Login</h1>
-            <h1>More than <span className='text-[#FF5454] font-semibold'>15000+ Recipes</span> around the world</h1>
-            <div className='flex items-center flex-col justify-center bg-[#ffd5d481] bg-white  backdrop-blur-sm w-[450px] rounded-lg'>
-                <form className='flex flex-col gap-10 px-10 py-20 '>
-                    <input type='mail' placeholder='Enter email ' className='py-3 w-72 rounded-md pl-2 placeholder-shown:bg-[#FFF4F3]' />
-                    <input type='password' placeholder='Enter password ' className='py-3 w-72 rounded-md pl-2 placeholder-shown:bg-[#FFF4F3]' />
-                    <button className='py-3 w-72 rounded-md bg-[#FF5454] text-white'>Login</button>
-                </form>
-            </div >
-
+        <div className='brightness-90  gap-3 h-[100vh] bg-hero bg-cover filter flex flex-col items-center justify-center' >
+            {!isSignin && <><h1 className='text-4xl text-black font-serif font-semibold py-3  '>Login</h1>
+                <h1>More than <span className='text-[#FF5454] font-semibold'>15000+ Recipes</span> around the world</h1>
+                <div className='flex items-center flex-col py-5 justify-center bg-[#ffd5d481] bg-white  backdrop-blur-sm w-[450px] rounded-lg'>
+                    <form className='flex flex-col gap-10 px-10 py-20 '>
+                        <input type='mail' placeholder='Enter email ' className='border-[#FF5454] border-b py-3 w-72 rounded-md pl-2 placeholder-shown:bg-[#FFF4F3]' />
+                        <input type='password' placeholder='Enter password ' className=' border-[#FF5454] border-b py-3 w-72 rounded-md pl-2 placeholder-shown:bg-[#FFF4F3]' />
+                        <button className='py-3 w-72 rounded-md bg-[#FF5454] text-white'>Login</button>
+                    </form>
+                    <h2>New user ? <span className='text-[#FF5454] cursor-pointer' onClick={() => {
+                        setSignin(true)
+                    }}>Register Now !!</span> </h2>
+                </div ></>}
+            {
+                isSignin && <><h1 className='text-4xl text-black font-serif font-semibold py-3  '>Sign In</h1>
+                    <h1>More than <span className='text-[#FF5454] font-semibold'>15000+ Recipes</span> around the world</h1>
+                    <div className='flex items-center flex-col py-5 justify-center bg-[#ffd5d481] bg-white  backdrop-blur-sm w-[450px] rounded-lg'>
+                        <form className='flex flex-col gap-10 px-10 py-20 '>
+                            <input type='text' placeholder='Enter name ' className='border-[#FF5454] border-b py-3 w-72 rounded-md pl-2 placeholder-shown:bg-[#FFF4F3]' />
+                            <input type='mail' placeholder='Enter email ' className='border-[#FF5454] border-b py-3 w-72 rounded-md pl-2 placeholder-shown:bg-[#FFF4F3]' />
+                            <input type='password' placeholder='Enter password ' className=' border-[#FF5454] border-b py-3 w-72 rounded-md pl-2 placeholder-shown:bg-[#FFF4F3]' />
+                            <button className='py-3 w-72 rounded-md bg-[#FF5454] text-white'>Login</button>
+                        </form>
+                        <h2>Already registered ? <span className='text-[#FF5454] cursor-pointer' onClick={() => {
+                            setSignin(false)
+                        }}>Sign In</span> </h2>
+                    </div ></>
+            }
         </div>
+
     )
 }
 
