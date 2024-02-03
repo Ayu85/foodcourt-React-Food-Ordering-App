8307f37039aae641c55ba32e5cb925b2101569dc
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-03 14:28:17 +0530

    responsive

--- foodcourt/src/components/Secondarysection.js
+++ foodcourt/src/components/Secondarysection.js
@@ -4,7 +4,7 @@ import delivery from "../assets/delivery2.png"
 import menufood from "../assets/menu.png"
 const Secondarysection = () => {
   return (
-    <div className='lg:flex-row justify-between lg:px-20 lg:pt-20 lg:py-10 bg-gradient-to-l from-[#f9dbce] md:flex md:flex-col items-center md:gap-44 '>
+    <div className='lg:flex-row justify-between lg:px-20 lg:pt-20 lg:py-10 bg-gradient-to-l from-[#f9dbce] md:flex md:flex-col items-center md:gap-36 md:pt-10 sm:pt-10 sm:flex sm:flex-col sm:gap-36 '>
       <div className='flex flex-col max-w-96 gap-4 mt-6 relative'>
         <h1 className='text-3xl font-bold'>Why we are best?</h1>
         <p1 className="italic ">Discover the excellence of our food ordering website. With seamless navigation, real-time updates, and a tantalizing array of culinary options, we redefine convenience and taste.
@@ -16,7 +16,7 @@ const Secondarysection = () => {
         <img src={instantfood} alt='instant food ' className='bg-[#fff8f4] z-20 w-48 h-72 rounded-3xl  shadow-[#fbaaa8]' />
         <img src={delivery} alt='instant food ' className=' bg-[#fff8f4] w-48 h-72 rounded-3xl mt-12  shadow-[#fba9a8a4]' />
         <img src={menufood} alt='instant food ' className='bg-[#fff8f4] w-48 h-72 rounded-3xl  shadow-[#fbaaa8]' />
-        <div className='absolute   text-9xl font-bold -bottom-12 w-max -left-20 text-[#fe7171] select-none'>Eat with ease</div>
+        <div className='absolute hidden md:block lg:block  text-9xl font-bold lg:-bottom-12 md:-bottom-5 w-max -left-20 text-[#fe7171] select-none'>Eat with ease</div>
       </div>
     </div>
   )
