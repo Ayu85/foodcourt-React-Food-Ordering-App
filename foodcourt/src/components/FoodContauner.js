64558daa9016e5bbe53bbd66cffddfe9e99da0b9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-03 21:45:55 +0530

    food container making

---
+++ foodcourt/src/components/FoodContauner.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const FoodContauner = () => {
+    return (
+        <div className='flex flex-col justify-center items-center'>
+            <div>
+                {/* box1 (heading and sub heading)*/}
+                <h1>Choose your favourite food !! </h1>
+                <p>Get your customized taste choosen amongt 15000+ top rated restaurants</p>
+            </div>
+            <div>
+                {/* box 2 */}
+                <div>Chicken</div>
+                <div>Burger</div>
+                <div> Pizza</div>
+                <div>Cakes</div>
+                <div>Snack</div>
+            </div>
+            <div>
+                {/* box 3 */}
+            </div>
+
+        </div>
+    )
+}
+
+export default FoodContauner
