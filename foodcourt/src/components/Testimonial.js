907c454791a19fe8452830a734ca18b8ec3b5bdc
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-04 14:58:33 +0530

    testimonials

--- foodcourt/src/components/Testimonial.js
+++ foodcourt/src/components/Testimonial.js
@@ -5,20 +5,20 @@ import { WALL3 } from '../utils/constants'
 import { WALL4 } from '../utils/constants'
 import { WALL5 } from '../utils/constants'
 import { WALL6 } from '../utils/constants'
-
+import lines from '../assets/lines.png'
 const Testimonial = () => {
     return (
-        <div className='flex px-20  pt-20 h-[90vh]'>
-            <div className='flex flex-wrap w-[65%] relative'>
-                <img src={WALL1} alt='logo' className='w-48 aspect-square rounded-full absolute top-32' />
-                <img src={WALL2} alt='logo' className='w-44 aspect-square rounded-full absolute top-72 right-96 shadow-2xl shadow-[#FF5454]' />
-                <img src={WALL3} alt='logo' className='w-32 aspect-square rounded-full absolute bottom-0' />
-                <img src={WALL4} alt='logo' className='w-40 aspect-square rounded-full absolute right-20 top-0' />
-                <img src={WALL5} alt='logo' className='w-44 aspect-square rounded-full absolute left-72 ' />
-                <img src={WALL6} alt='logo' className='w-40 aspect-square rounded-full absolute bottom-11 right-10' />
-
+        <div className='flex px-20  pt-20 h-[90vh] bg-[#f7f3f071] relative'>
+            <div className=' flex-wrap w-[65%] absolute hidden lg:flex'>
+                <img src={WALL1} alt='logo' className='w-48 aspect-square rounded-full absolute top-10' />
+                <img src={WALL2} alt='logo' className='w-44 aspect-square rounded-full absolute top-72 right-44 shadow-2xl shadow-[#FF5454]' />
+                <img src={WALL3} alt='logo' className='w-32 aspect-square rounded-full absolute top-80' />
+                <img src={WALL4} alt='logo' className='w-40 aspect-square rounded-full absolute right-44 top-10' />
+                <img src={WALL5} alt='logo' className='w-32 aspect-square rounded-full absolute  top-44 left-64  ' />
+                {/* <img src={WALL6} alt='logo' className='w-40 aspect-square rounded-full absolute bottom-28 right-10 ' /> */}
+                <img src={lines} alt='logo' className='absolute -z-10 w-full -left-20 ' />
             </div>
-            <div className='bg-[#fff2ea] w-[35%]'>
+            <div className='bg-[#fff2ea] w-[35%] absolute right-5 h-[90%] top-16 '>
                 <div>
                     <h1>2000+</h1>
                     <h2>Regular customers</h2>
