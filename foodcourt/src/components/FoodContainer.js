7b30c187ec06ca7c1f83ab82b8bd3fde109cf628
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-04 13:56:20 +0530

    food menu done

--- foodcourt/src/components/FoodContainer.js
+++ foodcourt/src/components/FoodContainer.js
@@ -37,29 +37,29 @@ const FoodContainer = () => {
                 <h1 className='text-3xl font-semibold'>Choose your favourite food !! </h1>
                 <p className='text-md text-slate-600 pt-1'>Get your customized taste choosen amongst 15000+ top rated restaurants</p>
             </div>
-            <div className='flex gap-3 pt-5'>
+            <div className='flex flex-wrap justify-center px-5 gap-3 pt-5'>
                 {/* box 2 */}
                 <div onClick={() => {
                     dispatch(toogleItem("burger"))
-                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                }} style={currentItem === 'burger' ? { backgroundColor: "#FF5454", color: "white", fontWeight: 'normal' } : {}} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
                  border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'>
                     <img src={burger} alt='logo' className='w-9' />Burger</div>
                 <div onClick={() => {
-                    dispatch(toogleItem("chicken"))
-                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
-                 border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'><img src={chicken} alt='logo' className='w-9' />Chicken</div>
-                <div onClick={() => {
                     dispatch(toogleItem("pizza"))
-                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                }} style={currentItem === 'pizza' ? { backgroundColor: "#FF5454", color: "white", fontWeight: 'normal' } : {}} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
                  border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'> <img src={pizza} alt='logo' className='w-9' />Pizza</div>
                 <div onClick={() => {
                     dispatch(toogleItem("cake"))
-                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                }} style={currentItem === 'cake' ? { backgroundColor: "#FF5454", color: "white", fontWeight: 'normal' } : {}} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
                  border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'><img src={cake} alt='logo' className='w-9' />Cakes</div>
                 <div onClick={() => {
                     dispatch(toogleItem("snack"))
-                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                }} style={currentItem === 'snack' ? { backgroundColor: "#FF5454", color: "white", fontWeight: 'normal' } : {}} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
                  border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'><img src={snack} alt='logo' className='w-9' />Snack</div>
+                <div onClick={() => {
+                    dispatch(toogleItem("chicken"))
+                }} style={currentItem === 'chicken' ? { backgroundColor: "#FF5454", color: "white", fontWeight: 'normal' } : {}} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                 border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'><img src={chicken} alt='logo' className='w-9' />Chicken</div>
             </div>
             {currentItem == 'burger' && <div className='flex flex-wrap gap-8 px-20 justify-center pt-9 '>
                 {/* box 3 */}
