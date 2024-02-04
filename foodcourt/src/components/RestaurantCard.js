c916887b33dd53f054827e9c96fc156bb260b257
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-04 11:47:26 +0530

    location icon

--- foodcourt/src/components/RestaurantCard.js
+++ foodcourt/src/components/RestaurantCard.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { IMAGE_API } from '../utils/constants'
+import { IoLocation } from "react-icons/io5";
 
 const RestaurantCard = ({ name, cloudinaryImageId, locality, areaName, costForTwo, avgRating }) => {
     return (
         <div className='flex flex-col py-2 px-2 gap-2 items-center card-font   shadow-[#facdcd8f] shadow-xl  bg-white justify-center '>
             <img src={IMAGE_API + cloudinaryImageId} alt='logo' className=' w-48 aspect-square   ' />
             <div className=' text-black  bottom-0 text-center   w-full rounded-3xl  '> <h1 className='text-lg font-semibold   bottom-5'>{name}</h1>
-                <h1 className=' text-md text-slate-700'>{locality},{areaName}   </h1></div>
+                <h1 className=' text-md text-slate-700 flex items-center justify-center gap-1  '>
+                    {locality},{areaName}<IoLocation className='text-[#FF5454]' />
+                </h1></div>
         </div>
     )
 }
