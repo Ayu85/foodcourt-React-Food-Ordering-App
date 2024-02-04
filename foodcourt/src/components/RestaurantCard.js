cd6e9782346cacca0968a7e64c98718abd38e879
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-04 13:06:49 +0530

    current items fetching

--- foodcourt/src/components/RestaurantCard.js
+++ foodcourt/src/components/RestaurantCard.js
@@ -4,7 +4,7 @@ import { IoLocation } from "react-icons/io5";
 
 const RestaurantCard = ({ name, cloudinaryImageId, locality, areaName, costForTwo, avgRating }) => {
     return (
-        <div className='flex flex-col py-2 px-2 gap-2 items-center card-font   shadow-[#facdcd8f] shadow-xl  bg-white justify-center '>
+        <div className='flex flex-col min-w-[220px] max-w-[220px] py-2 px-2 gap-2 items-center card-font   shadow-[#facdcd8f] shadow-xl  bg-white justify-center '>
             <img src={IMAGE_API + cloudinaryImageId} alt='logo' className=' w-48 aspect-square   ' />
             <div className=' text-black  bottom-0 text-center   w-full rounded-3xl  '> <h1 className='text-lg font-semibold   bottom-5'>{name}</h1>
                 <h1 className=' text-md text-slate-700 flex items-center justify-center gap-1  '>
