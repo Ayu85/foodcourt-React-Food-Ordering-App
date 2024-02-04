ae0816e325e72bc918ad1c905ac1ba4d2a7f48d7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-04 12:22:03 +0530

    slice

--- foodcourt/src/components/FoodContainer.js
+++ foodcourt/src/components/FoodContainer.js
@@ -9,7 +9,8 @@ import snack from "../assets/fastfood.png"
 import RestaurantCard from './RestaurantCard'
 import { mockBurgerData } from '../utils/__mocks__'
 import Shimmer from './Shimmer';
-
+import useDispatch from 'react-redux'
+import toogleItem from "../redux/slices/CurrentItem"
 const FoodContainer = () => {
     const [burgerdata, setBurger] = useState(null);
     useEffect(() => {
@@ -17,7 +18,7 @@ const FoodContainer = () => {
             setBurger(mockBurgerData)
         }, 5000)
     }, [])
-
+    const dispatch = useDispatch();
     return (
         <div className='flex flex-col justify-center items-center pt-10 bg-[#fff2ea]'>
             <div className='text-center'>
@@ -27,15 +28,26 @@ const FoodContainer = () => {
             </div>
             <div className='flex gap-3 pt-5'>
                 {/* box 2 */}
-                <div className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
-                 border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'><img src={burger} alt='logo' className='w-9' />Burger</div>
-                <div className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                <div onClick={() => {
+                    dispatch(toogleItem("burger"))
+                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                 border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'>
+                    <img src={burger} alt='logo' className='w-9' />Burger</div>
+                <div onClick={() => {
+                    dispatch(toogleItem("chicken"))
+                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
                  border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'><img src={chicken} alt='logo' className='w-9' />Chicken</div>
-                <div className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                <div onClick={() => {
+                    dispatch(toogleItem("pizza"))
+                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
                  border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'> <img src={pizza} alt='logo' className='w-9' />Pizza</div>
-                <div className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                <div onClick={() => {
+                    dispatch(toogleItem("cake"))
+                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
                  border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'><img src={cake} alt='logo' className='w-9' />Cakes</div>
-                <div className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
+                <div onClick={() => {
+                    dispatch(toogleItem("snack"))
+                }} className='px-4 py-3 rounded-xl font-semibold text-slate-700 text-lg cursor-pointer
                  border-slate-200 border flex items-center gap-1 hover:bg-[#ffd1bd9d] hover:border-[#FF5454] transition-all'><img src={snack} alt='logo' className='w-9' />Snack</div>
             </div>
             {!burgerdata ? <Shimmer /> : <div className='flex flex-wrap gap-8 px-20 justify-center pt-9 '>
