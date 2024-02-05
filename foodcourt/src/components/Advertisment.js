3332422f10ba32f378965889197eab3938e2ffbd
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-05 11:34:46 +0530

    ads page

--- foodcourt/src/components/Advertisment.js
+++ foodcourt/src/components/Advertisment.js
@@ -4,12 +4,12 @@ import playstore from '../assets/google-play-badge.png'
 import app from '../assets/application.png'
 const Advertisment = () => {
     return (
-        <wrapper className='flex justify-center items-center'>
-            <div className='flex'>
-                <div  className='flex flex-col gap-2'>
+        <wrapper className='flex justify-center items-center bg-gradient-to-b from-[#fff2ea]'>
+            <div className='flex gap-44 bg-white px-44 py-14 rounded-[100px]'>
+                <div className='flex flex-col gap-2'>
                     {/* left box */}
-                    <h1 className='text-3xl w-80 font-semibold'>Get upto 20% off on your first transaction on our app !</h1>
-                    <p className='italic w-96'>"Explore endless flavors with our food app! Easy ordering, quick delivery, and delicious meals await. Download now for culinary delight!"</p>
+                    <h1 className='text-3xl w-80 font-semibold text-[#00031F]'>Get upto 20% off on your first transaction on our app !</h1>
+                    <p className='italic w-96 text-[#434768]'>"Explore endless flavors with our food app! Easy ordering, quick delivery, and delicious meals await. Download now for culinary delight!"</p>
                     <buttonsection className='flex items-center'>
                         <img src={appstore} alt='logo' className='w-44 h-14' />
                         <img src={playstore} alt='logo' className='w-52 h-20 ' />
