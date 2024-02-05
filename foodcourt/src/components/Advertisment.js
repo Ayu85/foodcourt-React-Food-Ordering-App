b395970fee6464294d326f8ef4544da46a71a1ba
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-05 11:45:56 +0530

    styling

--- foodcourt/src/components/Advertisment.js
+++ foodcourt/src/components/Advertisment.js
@@ -2,10 +2,12 @@ import React from 'react'
 import appstore from '../assets/macstore.png'
 import playstore from '../assets/google-play-badge.png'
 import app from '../assets/application.png'
+import { FaDotCircle } from "react-icons/fa";
+
 const Advertisment = () => {
     return (
         <wrapper className='flex justify-center items-center bg-gradient-to-b from-[#fff2ea]'>
-            <div className='flex gap-44 bg-white px-44 py-14 rounded-[100px]'>
+            <div className='flex flex-wrap gap-44 overflow-hidden bg-white px-20 pt-14 rounded-[100px] border'>
                 <div className='flex flex-col gap-2'>
                     {/* left box */}
                     <h1 className='text-3xl w-80 font-semibold text-[#00031F]'>Get upto 20% off on your first transaction on our app !</h1>
@@ -15,10 +17,10 @@ const Advertisment = () => {
                         <img src={playstore} alt='logo' className='w-52 h-20 ' />
                     </buttonsection>
                 </div>
-                <div>
+                <div className='relative flex'>
                     {/* right box */}
-                    <img src={app} alt='logo' className=' w-72' />
-
+                    <img src={app} alt='logo' className=' w-[450px] z-40' />
+                    <FaDotCircle className='absolute  text-9xl -right-32 top-20 text-[#ffd5c3] '/>
                 </div>
             </div>
         </wrapper>
