fe0ee0e15a582351747c95156b8ae3617fa4ab4a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-05 15:07:06 +0530

    copy right

---
+++ foodcourt/src/components/Copyright.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Copyright = () => {
+    return (
+        <div>
+           ©️ Copyright Aayush singh 2024. All Rights Reserved !
+        </div>
+    )
+}
+
+export default Copyright
