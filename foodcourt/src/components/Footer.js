37a386aff724ce75353e931523aaae9b7efccd04
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-05 15:05:58 +0530

    footer done

--- foodcourt/src/components/Footer.js
+++ foodcourt/src/components/Footer.js
@@ -7,15 +7,15 @@ import { FaFacebookSquare } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <wrapper className='flex items-center justify-center'>
-            <div className='flex px-10 gap-20 pt-10'>
+        <wrapper className='flex items-center justify-center pt-5 pb-5'>
+            <div className='flex px-16 gap-20 pt-10 border-b border-slate-300 rounded-[200px] pb-10'>
                 {/* footer main box */}
                 <div className='flex flex-col gap-2 w-72'>
                     {/* box1 */}
                     <img src={logo} alt='logo' className='w-36' />
                     <p className='text-[#434768] italic'> Discover a world of flavors, from savory to sweet, all in one place. Whether you're craving classics or eager to explore new tastes,
                         our app brings the best of dining right to your fingertips. Bon appétit!"</p>
-                    <profiles className='flex gap-1 text-[#FF5454]'>
+                    <profiles className='flex gap-1 text-[#FF5454] pt-2'>
                         <FaLinkedin />
                         <RiInstagramFill />
                         <FaFacebookSquare />
@@ -25,7 +25,7 @@ const Footer = () => {
                 <div className='flex flex-col gap-6'>
                     {/* box2 */}
                     <h1 className='font-semibold text-lg text-[#00031f]'>About us</h1>
-                    <ul className='list-none text-[#434768] flex flex-col gap-3 cursor-pointer'> <li>About us</li>
+                    <ul className='list-none text-[#434768] flex flex-col gap-3 cursor-pointer text-sm'> <li>About us</li>
                         <li>Features</li>
                         <li>News </li>
                         <li>Menu</li>
@@ -34,7 +34,7 @@ const Footer = () => {
                 <div className='flex flex-col gap-6'>
                     {/* box3 */}
                     <h1 className='font-semibold text-lg text-[#00031f]'>Company</h1>
-                    <ul className='list-none text-[#434768] flex flex-col gap-3 cursor-pointer'> <li>Why food court?</li>
+                    <ul className='list-none text-[#434768] flex flex-col gap-3 cursor-pointer text-sm'> <li>Why food court?</li>
                         <li>Partner with us</li>
                         <li>FAQ</li>
                         <li>Blog</li></ul>
@@ -42,7 +42,7 @@ const Footer = () => {
                 <div className='flex flex-col gap-6'>
                     {/* box4 */}
                     <h1 className='font-semibold text-lg text-[#00031f]'>Support</h1>
-                    <ul className='list-none text-[#434768] flex flex-col gap-3 cursor-pointer'> <li>Contact</li>
+                    <ul className='list-none text-[#434768] flex flex-col gap-3 cursor-pointer text-sm'> <li>Contact</li>
                         <li>Account</li>
                         <li>Feedback </li>
                         <li>Other queries</li></ul>
@@ -50,7 +50,7 @@ const Footer = () => {
                 <div className='flex flex-col gap-6'>
                     {/* box5*/}
                     <h1 className='font-semibold text-lg text-[#00031f]'>Get In Touch</h1>
-                    <ul  className='list-none text-[#434768] flex flex-col gap-3 cursor-pointer'>    <li>Questions or feedback !</li>
+                    <ul  className='list-none text-[#434768] flex flex-col gap-3 cursor-pointer text-sm'>    <li>Questions or feedback !</li>
                         <li>We,d love to hear from you !</li></ul>
 
                 </div>
